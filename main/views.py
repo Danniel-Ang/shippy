4296e2afddf5b573a1d9ad199c6996883f023841
Danniel-Ang <danniel@ui.ac.id>
2024-09-09 14:27:37 +0700

    Adding product

--- main/views.py
+++ main/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 def show_main(request):
+    p = Product(name = "Danniel", price = 99999, description = "The best product ever")
+    p.save()
     context = {
-        'name' : 'Danniel',
+        'name' : p.name,
         'pbb_class' : 'E',
         'student_id' : '2306152090'        
     }
