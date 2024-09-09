44e0ea4899069c837f2e00420e6f939ddfb2addf
Danniel-Ang <danniel@ui.ac.id>
2024-09-09 13:50:03 +0700

    Forgot the hosts hahah

--- shippy/settings.py
+++ shippy/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-j!3-jzqpwvntx-aefvo$w%3c4peu$z5142dbjy=!$q=ugd)w0n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
-
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "danniel-shippy.pbp.cs.ui.ac.id"]
 
 # Application definition
 
